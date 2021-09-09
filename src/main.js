e11d8820db3e733ee8630a7792de870e6ef7dd05
qjd <qjd1774@163.com>
2021-09-09 09:16:49 +0800

    feat: 新增马赛克处理 --qian

--- src/main.js
+++ src/main.js
@@ -19,8 +19,13 @@ image.onload = draw;
 function draw() {
   const { width, height } = image;
   ctx.drawImage(image, 0, 0, width, height);
+  ctx.font = "20px blod pingfang";
+  // 灰度用到ImageData
   let imageDt = ctx.getImageData(0, 0, width, height);
   const data = imageDt.data;
+  // 创建一个马赛克副本
+  let imageDTM = ctx.getImageData(0, 0, width, height);
+  const datam = imageDTM.data;
   // 灰度
   for (let i = 0; i < data.length; i += 4) {
     const [r, g, b] = [data[i], data[i + 1], data[i + 2]];
@@ -30,4 +35,23 @@ function draw() {
     data[i + 2] = ml;
   }
   ctx.putImageData(imageDt, 0, height);
+  // 马赛克
+  // 被打码出尺寸大小
+  const size = 10;
+  ctx.putImageData(imageDTM, width, 0);
+  // 列循环加行循环
+  for (let y = 0; y < height; y += size) {
+    for (let x = 0; x < width; x += size) {
+      const ind = (y * width + x) * 4; // 获取每一个像素的r索引位置
+      const [r, g, b] = [datam[ind], datam[ind + 1], datam[ind + 2]];
+      ctx.fillStyle = `RGB(${r},${g},${b})`;
+      // 每一次绘画一个格子到第二个图形中
+      ctx.fillRect(x + width, y, size, size);
+    }
+  }
+  ctx.fillStyle = "#fff";
+  ctx.textBaseline = "top"; // 顶部对其
+  ctx.fillText("原图", 0, 0); // 第一张图
+  ctx.fillText("灰度处理", 0,  height); // 第一张图
+  ctx.fillText("马赛克", width, 0); // 第一张图
 }
