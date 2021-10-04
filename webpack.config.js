602f562a231f3698e72f60b3ea7a2defa6c9aa72
qjd <qjd1774@163.com>
2021-10-04 21:35:58 +0800

    feat(less-loader): add less-loader less

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,16 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 module.exports = {
   entry: {
-    canvas: path.resolve(__dirname, "src/canvas.js"),
+    canvas: path.resolve(__dirname, "src/canvas.js"), // 相对路径和绝对路径都可以
     drag: path.resolve(__dirname, "src/drag.js"),
     audio: path.resolve(__dirname, "src/audio.js"),
+    less: path.resolve(__dirname, "src/less.js"),
   },
   mode: "development",
   devtool: "eval-source-map",
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "dist"), // 要求绝对路径
     clean: true,
   },
   resolve: {
@@ -26,6 +27,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.less$/i,
+        use: ["style-loader", "css-loader","less-loader"],
       },
       {
         // 处理图片文件
@@ -67,6 +72,15 @@ module.exports = {
       filename: "audio.html",
       hash: true,
       chunks: ["audio"],
+      minify: {
+        collapseInlineTagWhitespace: true, // 折叠空白压缩代码
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "public/less.html",
+      filename: "less.html",
+      hash: true,
+      chunks: ["less"],
       minify: {
         collapseInlineTagWhitespace: true, // 折叠空白压缩代码
       },
