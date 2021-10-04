0a72ad58eccdb716f9e8260c750fb5f92fb9a90c
qjd <qjd1774@163.com>
2021-10-05 05:49:02 +0800

    docs: 注释

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = {
       "@babel/preset-env",
       {
         // "usage" | "entry" | false 默认false
-        useBuiltIns: "entry", // usage==>依据代码来实现转换不管浏览器，entry==>依据浏览器需要什么依赖,需要入口文件引入相关依赖
+        useBuiltIns: "entry", // usage==>依据源代码所需要的新语法进行填充，entry==>依据浏览器需要什么依赖,需要入口文件引入相关依赖
         corejs: 3, // 默认版本是2
       },
     ],
